6d2742bce138afbff438b0e064b9cef1df59ce5b
xuhaiqiang <xuhaiqiang@xiaoheiban.cn>
2021-07-23 11:54:32 +0800

    feat: s

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import BatchState from './routes/BatchState';
 
 // legacy 旧模式
-// ReactDOM.render(<h1>hello</h1>, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <ul>
+      <li>
+        <Link to="/BatchState">BatchState</Link>
+      </li>
+    </ul>
+    <Route path="/BatchState" component={BatchState} />
+  </Router>,
+  document.getElementById('root')
+);
 
 // 并发渲染
-ReactDOM.createRoot(document.getElementById('root')!).render(<h1>hello</h1>);
+// ReactDOM.createRoot(document.getElementById('root')!).render(<h1>hello</h1>);
